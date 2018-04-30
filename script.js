f10a43afd5c7dffce72b159fe6005417543c5cae
Zachary Moss <zachary.moss@ymail.com>
2018-04-30 16:15:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+    //console.log(array.length);
+}
+
+preloadImages(["images/background1.jpg", "images/me3.jpg"]);

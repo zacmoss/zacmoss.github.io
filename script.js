b01d0f2c6ae94d79a4e464d470d8c70dc1a6be15
Zachary Moss <zachary.moss@ymail.com>
2018-05-03 14:42:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,40 @@ function preloadImages(array) {
 }
 
 preloadImages(["images/background1.jpg", "images/me3.jpg"]);
+
+
+
+
+
+
+function mobilizeEducation(x) {
+    if (x.matches) { // If media query matches
+        document.getElementById("educationContainer2").classList.remove("row");
+        //document.body.style.backgroundColor = "yellow";
+    } else {
+        document.getElementById("educationContainer2").classList.add("row");
+        //document.body.style.backgroundColor = "pink";
+    }
+}
+
+var x = window.matchMedia("(max-width: 800px)")
+mobilizeEducation(x) // Call listener function at run time
+x.addListener(mobilizeEducation) // Attach listener function on state changes
+
+
+
+
+function mobilizeFooter(x) {
+    if (x.matches) { // If media query matches
+        document.getElementById("contactContainer").classList.remove("row");
+        document.getElementById("mobileContactDiv").classList.add("row", "space-around");
+        //document.body.style.backgroundColor = "yellow";
+    } else {
+        document.getElementById("contactContainer").classList.add("row");
+        //document.body.style.backgroundColor = "pink";
+    }
+}
+
+var x = window.matchMedia("(max-width: 930px)")
+mobilizeFooter(x) // Call listener function at run time
+x.addListener(mobilizeFooter) // Attach listener function on state changes
